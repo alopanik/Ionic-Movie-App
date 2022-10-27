436350eb09f22f788e470d7ea6e4e5b76b896d67
alopanik <alopanik@gmail.com>
2022-10-26 22:36:58 -0400

    App built, initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '3d2e71868169b7ef95c7db3286e42f7d',
+  baseURL: 'https://api.themoviedb.org/3',
+  images: 'https://image.tmdb.org/t/p'
 };
 
 /*
